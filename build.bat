84531ea65bb09c0b5f3684571675c981dee5b869
Волощик Никита Анатольевич <nikita_v@corp.shaft-gear.ru>
2025-10-23 15:07:53 +0300

    Keep only necessary files

--- build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Building Doom Clock Screensaver...
-python -m PyInstaller --onefile --windowed --name="Doom Clock" --add-data="doom2016left.ttf;." doom_clock.py
-
-echo.
-echo Build complete!
-echo Now rename "dist\Doom Clock.exe" to "Doom Clock.scr"
-echo.
-pause
